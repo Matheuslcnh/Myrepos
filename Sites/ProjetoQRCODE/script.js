3e361048c22527a72476381c0bd4194a6782de3a
Matheuslcnh <matheus.cav.san@gmail.com>
2024-12-09 21:33:54 -0300

    new

---
+++ Sites/ProjetoQRCODE/script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+
+
+
+function generateQrCode(){
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if(!qrCodeInputValue) return;
+    qrCodeBtn.innerText = "Gerando Código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load", ()=>{
+        container.classList.add("active");
+        qrCodeBtn.innerText = "Código Criado!";
+    })
+
+}
+qrCodeBtn.addEventListener("click" , ()=>{
+    generateQrCode();
+})
+
+qrCodeInput.addEventListener("Keydown" , (e)=>{
+    if(e.code === "Enter"){
+        generateQrCode();
+    }
+})
+
+qrCodeInput.addEventListener("keyup", ()=>{
+    if(!qrCodeInput.value){
+        container.classList.remove("active");
+        qrCodeBtn.innerText = "Gerar QR CODE!"
+    }
+})
