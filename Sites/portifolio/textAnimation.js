050fccc84d50d481099e417a297e73d7fc32bab0
Matheus Lourenço Cunha <matheus.cav.san@gmail.com>
2025-09-30 14:04:22 -0300

    new

--- Sites/portifolio/textAnimation.js
+++ Sites/portifolio/textAnimation.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const typedTextSpan = document.querySelector(".texto-animado");
-    const textArray = ["Back-end", "Criativo", "Web"];
+    const textArray = ["Back-end", "Criativo", "Web", "Front-end"];
     const typingDelay = 100;
     const erasingDelay = 150;
     const newTextDelay = 1000; 
